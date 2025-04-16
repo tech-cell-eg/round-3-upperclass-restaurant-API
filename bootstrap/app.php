aa40530bba02986565a677630cfae96925768464
Tarek Hesham <tarekghost6@gmail.com>
2025-04-16 20:49:42 +0200

    [Exception] - added two exceptions to handel 404 res

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +17,19 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (ModelNotFoundException $e, $request) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Resource not found',
+                'data' => null
+            ], 404);
+        });
+    
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Endpoint not found',
+                'data' => null
+            ], 404);
+        });
     })->create();
