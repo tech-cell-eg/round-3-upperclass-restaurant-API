c1c2bd9c3fa0fec7aefbddcbd31091c2eec5a97b
Tarek Hesham <tarekghost6@gmail.com>
2025-04-16 15:15:40 +0200

    [Traits] - Add ApiResponse trait

--- app/Http/Controllers/Api/MenuItemsController.php
+++ app/Http/Controllers/Api/MenuItemsController.php
@@ -5,16 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\MenuItemResource;
 use App\Models\MenuItem;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 
 class MenuItemsController extends Controller
 {
+    use ApiResponse;
+
     public function index()
     {
-        return response()->json([
-            'status' => true,
-            'message' => 'Fetched successfully',
-            'data' => MenuItemResource::collection(MenuItem::all())
-        ]);
+        return $this->successResponse(MenuItemResource::collection(MenuItem::all()), 'Menu Items');
     }
 }
