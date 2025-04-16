85c2cea5cc9f3037dde2f209a426fa2d2bd0893c
Tarek Hesham <tarekghost6@gmail.com>
2025-04-16 20:37:05 +0200

    [GiftCard] - Make migrations.

---
+++ database/migrations/2025_04_16_172754_create_gift_card_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gift_card', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->decimal('price', 8, 2);
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gift_card');
+    }
+};
